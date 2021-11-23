0b4dde500e94f915a69394d800545d8a8496b9aa
Dominic <dominicjosephenriquez@gmail.com>
2021-11-23 11:40:17 +0800

    Started SingASong refactoring kata

---
+++ SingASongKata/SingASongTests/SingASongTests.cs
@@ -0,0 +1,67 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using song;
+using System;
+using System.IO;
+using System.Text;
+
+namespace SingASongTests
+{
+    [TestFixture]
+    [UseReporter(typeof(DiffReporter))]
+    public class SingASongTests
+    {
+        [Test]
+        public void WhenRunProgramMain_OutputWholeSongToConsole()
+        {
+            string expected = @"There was an old lady who swallowed a fly.
+I don't know why she swallowed a fly - perhaps she'll die!
+
+There was an old lady who swallowed a spider;
+That wriggled and wiggled and tickled inside her.
+She swallowed the spider to catch the fly;
+I don't know why she swallowed a fly - perhaps she'll die!
+
+There was an old lady who swallowed a bird;
+How absurd to swallow a bird.
+She swallowed the bird to catch the spider,
+She swallowed the spider to catch the fly;
+I don't know why she swallowed a fly - perhaps she'll die!
+
+There was an old lady who swallowed a cat;
+Fancy that to swallow a cat!
+She swallowed the cat to catch the bird,
+She swallowed the bird to catch the spider,
+She swallowed the spider to catch the fly;
+I don't know why she swallowed a fly - perhaps she'll die!
+
+There was an old lady who swallowed a dog;
+What a hog, to swallow a dog!
+She swallowed the dog to catch the cat,
+She swallowed the cat to catch the bird,
+She swallowed the bird to catch the spider,
+She swallowed the spider to catch the fly;
+I don't know why she swallowed a fly - perhaps she'll die!
+
+There was an old lady who swallowed a cow;
+I don't know how she swallowed a cow!
+She swallowed the cow to catch the dog,
+She swallowed the dog to catch the cat,
+She swallowed the cat to catch the bird,
+She swallowed the bird to catch the spider,
+She swallowed the spider to catch the fly;
+I don't know why she swallowed a fly - perhaps she'll die!
+
+There was an old lady who swallowed a horse...
+...She's dead, of course!";
+
+            StringBuilder output = new StringBuilder();
+            Console.SetOut(new StringWriter(output));
+
+            Program.Main(new string[] { });
+
+            Approvals.Verify(output);
+        }
+    }
+}
---
+++ SingASongKata/song/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace song
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> animals = new List<string> { "fly", "spider", "bird", "cat", "dog", "cow", "horse" };
+            Console.WriteLine(GenerateSong(animals));
+        }
+
+        private static string GenerateSong(List<string> animals)
+        {
+
+            string oldLadySwallowed = "There was an old lady who swallowed a ";
+            string dontKnowWhySheSwallowed = "I don't know why she swallowed a ";
+
+            return
+                oldLadySwallowed + "fly."
+                + "\n" +
+                dontKnowWhySheSwallowed + "fly - perhaps she'll die!"
+
+                + "\n\n" +
+
+                oldLadySwallowed + "spider;"
+                + "\n" +
+                "That wriggled and wiggled and tickled inside her."
+                + "\n" +
+                "She swallowed the spider to catch the fly;"
+                + "\n" +
+                dontKnowWhySheSwallowed + "fly - perhaps she'll die!"
+
+                + "\n\n" +
+
+                oldLadySwallowed + "bird;"
+                + "\n" +
+                "How absurd to swallow a bird."
+                + "\n" +
+                "She swallowed the bird to catch the spider,"
+                + "\n" +
+                "She swallowed the spider to catch the fly;"
+                + "\n" +
+                dontKnowWhySheSwallowed + "fly - perhaps she'll die!"
+
+                + "\n\n" +
+
+                oldLadySwallowed + "cat;"
+                + "\n" +
+                "Fancy that to swallow a cat!"
+                + "\n" +
+                "She swallowed the cat to catch the bird,"
+                + "\n" +
+                "She swallowed the bird to catch the spider,"
+                + "\n" +
+                "She swallowed the spider to catch the fly;"
+                + "\n" +
+                dontKnowWhySheSwallowed + "fly - perhaps she'll die!"
+
+                + "\n\n" +
+
+                oldLadySwallowed + "dog;"
+                + "\n" +
+                "What a hog, to swallow a dog!"
+                + "\n" +
+                "She swallowed the dog to catch the cat,"
+                + "\n" +
+                "She swallowed the cat to catch the bird,"
+                + "\n" +
+                "She swallowed the bird to catch the spider,"
+                + "\n" +
+                "She swallowed the spider to catch the fly;"
+                + "\n" +
+                dontKnowWhySheSwallowed + "fly - perhaps she'll die!"
+
+                + "\n\n" +
+
+                oldLadySwallowed + "cow;"
+                + "\n" +
+                "I don't know how she swallowed a cow!"
+                + "\n" +
+                "She swallowed the cow to catch the dog,"
+                + "\n" +
+                "She swallowed the dog to catch the cat,"
+                + "\n" +
+                "She swallowed the cat to catch the bird,"
+                + "\n" +
+                "She swallowed the bird to catch the spider,"
+                + "\n" +
+                "She swallowed the spider to catch the fly;"
+                + "\n" +
+                dontKnowWhySheSwallowed + "fly - perhaps she'll die!"
+
+                + "\n\n" +
+
+                oldLadySwallowed + "horse..."
+                + "\n" +
+                "...She's dead, of course!";
+        }
+    }
+}
